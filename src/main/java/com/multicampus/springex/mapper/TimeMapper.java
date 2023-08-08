7274219537e176620f14d7f51cc8bbfd110c4033
Kimminseong <alstjd1018@naver.com>
2023-08-08 09:46:48 +0900

    test commit

---
+++ src/main/java/com/multicampus/springex/mapper/TimeMapper.java
@@ -0,0 +1,8 @@
+package com.multicampus.springex.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper { //데이터베이스의 현재시간을 문자열로 받아와서 처리
+    @Select("select now()")
+    String getTime();
+}
---
+++ src/main/java/com/multicampus/springex/sample/SampleDAO_Oracle_Impl.java
@@ -0,0 +1,13 @@
+package com.multicampus.springex.sample;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Qualifier("oracle")
+public class SampleDAO_Oracle_Impl implements SampleDAO{
+
+
+
+}
